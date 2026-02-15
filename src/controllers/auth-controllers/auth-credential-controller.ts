47cbfcd0c812bbabd311ef0b7efc21a3fb9bb307
joydeep-git <joydeep.accounts@tuta.io>
2026-02-16 00:04:21 +0530

    Refactor login and password reset flows: improved OTP verification, user data fetching, and error handling

--- src/controllers/auth-controllers/auth-credential-controller.ts
+++ src/controllers/auth-controllers/auth-credential-controller.ts
@@ -78,45 +78,57 @@ class AuthCredentialControllers {
 
       if (missingField) return next(errRes(`${missingField} is required!`, StatusCode.BAD_REQUEST));
 
+
+
+      // check email
       if (!isValidEmail(email)) return next(errRes("Invalid Email!", StatusCode.BAD_REQUEST));
 
+
+
+      // verify otp
       const otpVerified = await authRedis.verifyOtp({ email, otp, type: "login" });
 
       if (!otpVerified) {
         return next(errRes("Incorrect OTP!", StatusCode.UNAUTHORIZED));
       }
 
-      let user: UserDataType | null = await authRedis.getUserData(email);
 
-      if (!user) user = await authQueries.findUser({ type: "email", value: email, getPassword: true });
+      // get account details
+      let fetchUserData: UserDataType | null = await authQueries.findUser({ type: "email", value: email, getPassword: true });
 
-      if (!user) return next(errRes("No Account Found!", StatusCode.BAD_REQUEST));
+      if (!fetchUserData) return next(errRes("No Account Found!", StatusCode.BAD_REQUEST));
 
-      const isSamePassword = await argon2.verify(user.password!, password);
+
+      // checking password
+      const isSamePassword = await argon2.verify(fetchUserData?.password!, password);
 
       if (!isSamePassword) return next(errRes("Incorrect Password!", StatusCode.UNAUTHORIZED));
 
 
+
       // create token
-      const token = authToken.cookieGenerator(user.id);
+      const token = authToken.cookieGenerator(fetchUserData.id);
 
       if (!token) return next(errRes("Unable to login! Please try again.", StatusCode.INTERNAL_SERVER_ERROR));
 
-      const userWithoutPassword = { ...user };
+      if (fetchUserData?.password) delete fetchUserData.password;
 
-      delete userWithoutPassword.password;
 
-      res
+      // delete OTP
+      await authRedis.deleteOtp({ email, type: "login" });
+
+
+      // FINAL
+      return res
         .status(StatusCode.OK)
         .cookie("token", token, authToken.cookieConfig())
         .json(
           {
             message: "Logged in successfully!",
-            data: userWithoutPassword,
+            data: fetchUserData,
           }
-        )
+        );
 
-      return await authRedis.deleteOtp({ email, type: "login" });
 
     } catch (err) {
 
@@ -127,6 +139,7 @@ class AuthCredentialControllers {
   }
 
 
+
   // forget password + delete OTP
   public async forgetPasswordUpdate(req: Request, res: Response, next: NextFunction) {
 
@@ -134,27 +147,37 @@ class AuthCredentialControllers {
 
       const { email, otp, password } = req.body;
 
+
+      // check email
       if (!isValidEmail(email)) return next(errRes("Invalid Email!", StatusCode.BAD_REQUEST));
 
+
+      // verify otp
       const verified = await authRedis.checkPasswordReset({ email, otp });
 
       if (!verified) return next(errRes("OTP unverified or expired!", StatusCode.BAD_REQUEST));
 
-      let user: UserDataType | null = await authRedis.getUserData(email);
 
-      if (!user) user = await authQueries.findUser({ type: "email", value: email, getPassword: false });
+      // fetch user data
+      let user: UserDataType | null = await authQueries.findUser({ type: "email", value: email, getPassword: false });
 
-      const update = await authQueries.updateSingleValue({ identifier: email, field: "password", value: password });
+      if (!user) return next(errRes("No account found!", StatusCode.NOT_FOUND));
 
-      if (update) {
 
+      // update user password
+      const isUpdated = await authQueries.updateSingleValue({ identifier: email, field: "password", value: password });
+
+      if (isUpdated) {
+
+        res.status(StatusCode.OK).json({
+          message: "Password Changed!"
+        });
+
+
+        // delete remaining redis cache
         await authRedis.deleteOtp({ email, type: "forget-password" });
-
         await authRedis.deletePasswordReset({ email, otp });
 
-        return res.status(StatusCode.OK).json({
-          message: "Password Changed!"
-        })
 
       } else {
 
@@ -169,18 +192,31 @@ class AuthCredentialControllers {
   }
 
 
+
+
+
   public async forgetPasswordVerification(req: Request, res: Response, next: NextFunction) {
 
     try {
 
       const { email, otp } = req.body;
 
+
+      // check email
+      if (!isValidEmail(email)) return next(errRes("Invalid Email!", StatusCode.BAD_REQUEST));
+
+
+      // verify otp
       const verified = await authRedis.verifyOtp({ email, otp, type: "forget-password" });
 
-      if (!verified) return next(errRes("Incorrect or expired OTP", StatusCode.UNAUTHORIZED));
+      if (!verified) return next(errRes("Invalid or expired OTP", StatusCode.UNAUTHORIZED));
 
+
+      // set user as verified for password reset
       await authRedis.setPasswordReset({ email, otp });
 
+
+      // send message to send new password
       return res.status(StatusCode.OK).json({ success: true, message: "OTP verified" });
 
     } catch (err) {
